f2715b700efe2065e351b1551998631abdb8005f
gtn777 <gtn777@orange.fr>
2023-04-09 04:43:40 +0200

    report microservice first commit

---
+++ reportservice/src/main/java/mediscreen/reportservice/ReportserviceApplication.java
@@ -0,0 +1,13 @@
+package mediscreen.reportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportserviceApplication.class, args);
+	}
+
+}
---
+++ reportservice/src/test/java/mediscreen/reportservice/ReportserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package mediscreen.reportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
